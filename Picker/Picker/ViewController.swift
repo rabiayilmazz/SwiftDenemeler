f0160da9d1269f6e539ae8c8e9d1c1ff480d9241
rabia yılmaz <45205525+rabiayilmazz@users.noreply.github.com>
2021-04-08 23:50:01 +0300

    Picker uygulama denemesi

---
+++ Picker/Picker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Picker
+//
+//  Created by Rabia on 22.03.2021.
+//  Copyright © 2021 Rabia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Picker/PickerTests/PickerTests.swift
@@ -0,0 +1,36 @@
+//
+//  PickerTests.swift
+//  PickerTests
+//
+//  Created by Rabia on 22.03.2021.
+//  Copyright © 2021 Rabia. All rights reserved.
+//
+
+import XCTest
+@testable import Picker
+
+class PickerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
