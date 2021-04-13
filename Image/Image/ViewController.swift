8164a2e2d0843b67c45a2e5942afce4570e240a2
rabia yılmaz <45205525+rabiayilmazz@users.noreply.github.com>
2021-04-13 22:05:53 +0300

    image ekleme projesi

---
+++ Image/Image/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Image
+//
+//  Created by Rabia on 9.04.2021.
+//  Copyright © 2021 Rabia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var buton: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
