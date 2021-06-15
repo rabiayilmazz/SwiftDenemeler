1b7d1af99e0ae2a27882fb09aa12ac983a16e727
rabia yılmaz <rabiarabiabbb@gmail.com>
2021-06-15 19:15:40 +0300

    mobil uygulama

---
+++ MobilApp/MobilApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MobilApp
+//
+//  Created by Rabia on 30.05.2021.
+//  Copyright © 2021 Rabia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
